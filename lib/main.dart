e4783c7ff8e6dfa35609d97fbdcb061e8804cc06
Joehunt <johan_newdestiny@example.com.my>
2021-08-08 15:01:59 +0800

    Fixed Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 // gumbale.utuban@gmail.com
 import 'package:chatflutter_firebase/screen/auth_screen.dart';
-import 'package:chatflutter_firebase/screen/chat_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/material.dart';
+
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,25 +12,24 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Chat',
+      title: 'FlutterChat',
       theme: ThemeData(
-          primarySwatch: Colors.pink,
-          backgroundColor: Colors.pink,
-          accentColor: Colors.deepPurple,
-          accentColorBrightness: Brightness.dark,
-          buttonTheme: ButtonTheme.of(context).copyWith(
-            buttonColor: Colors.pink,
-            textTheme: ButtonTextTheme.primary,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-          )),
+        primarySwatch: Colors.pink,
+        backgroundColor: Colors.pink,
+        accentColor: Colors.deepPurple,
+        accentColorBrightness: Brightness.dark,
+        buttonTheme: ButtonTheme.of(context).copyWith(
+          buttonColor: Colors.pink,
+          textTheme: ButtonTextTheme.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
       home: AuthScreen(),
-      // home: ChatScreen(),
     );
   }
 }
--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
-  AuthForm(this.submitFn);
+  AuthForm(
+    this.submitFn,
+    this.isLoading,
+  );
 
+  final bool isLoading;
   final void Function(
     String email,
     String password,
-    String username,
+    String userName,
     bool isLogin,
+    BuildContext ctx,
   ) submitFn;
 
   @override
@@ -15,11 +20,10 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
-  final GlobalKey<FormState> _formKey = GlobalKey();
-
+  final _formKey = GlobalKey<FormState>();
   var _isLogin = true;
-  var _userName = '';
   var _userEmail = '';
+  var _userName = '';
   var _userPassword = '';
 
   void _trySubmit() {
@@ -28,7 +32,13 @@ class _AuthFormState extends State<AuthForm> {
 
     if (isValid) {
       _formKey.currentState.save();
-      widget.submitFn(_userName, _userEmail, _userPassword, _isLogin);
+      widget.submitFn(
+        _userEmail.trim(),
+        _userPassword.trim(),
+        _userName.trim(),
+        _isLogin,
+        context,
+      );
     }
   }
 
@@ -44,17 +54,19 @@ class _AuthFormState extends State<AuthForm> {
               key: _formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
-                children: [
+                children: <Widget>[
                   TextFormField(
                     key: ValueKey('email'),
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(labelText: 'Email'),
                     validator: (value) {
                       if (value.isEmpty || !value.contains('@')) {
-                        return 'Invalid email!';
+                        return 'Please enter a valid email address.';
                       }
                       return null;
                     },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email address',
+                    ),
                     onSaved: (value) {
                       _userEmail = value;
                     },
@@ -62,55 +74,52 @@ class _AuthFormState extends State<AuthForm> {
                   if (!_isLogin)
                     TextFormField(
                       key: ValueKey('username'),
-                      decoration: InputDecoration(labelText: 'Username'),
                       validator: (value) {
-                        if (value.isEmpty || value.length < 5) {
-                          return 'Username too short!';
+                        if (value.isEmpty || value.length < 4) {
+                          return 'Please enter at least 4 characters';
                         }
                         return null;
                       },
+                      decoration: InputDecoration(labelText: 'Username'),
                       onSaved: (value) {
                         _userName = value;
                       },
                     ),
                   TextFormField(
                     key: ValueKey('password'),
-                    decoration: InputDecoration(labelText: 'Password'),
-                    obscureText: true, // ** marking for password
                     validator: (value) {
-                      if (value.isEmpty || value.length < 5) {
-                        return 'Password is too short!';
+                      if (value.isEmpty || value.length < 7) {
+                        return 'Password must be at least 7 characters long.';
                       }
                       return null;
                     },
+                    decoration: InputDecoration(labelText: 'Password'),
+                    obscureText: true,
                     onSaved: (value) {
                       _userPassword = value;
                     },
                   ),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
+                  SizedBox(height: 12),
+                  if (widget.isLoading) CircularProgressIndicator(),
+                  if (!widget.isLoading)
+                    ElevatedButton(
+                      child: Text(_isLogin ? 'Login' : 'Signup'),
+                      onPressed: _trySubmit,
+                    ),
+                  if (!widget.isLoading)
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        primary: Theme.of(context).primaryColor,
                       ),
-                    ),
-                    onPressed: _trySubmit,
-                    child: Text(_isLogin ? 'Login' : 'SignUp'),
-                  ),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      primary: Theme.of(context).primaryColor,
-                    ),
-                    child:
-                        Text(_isLogin ? 'Create new acct' : 'I have account'),
-                    onPressed: () {
-                      setState(() {
-                        _isLogin = !_isLogin;
-                      });
-                    },
-                  )
+                      child: Text(_isLogin
+                          ? 'Create new account'
+                          : 'I already have an account'),
+                      onPressed: () {
+                        setState(() {
+                          _isLogin = !_isLogin;
+                        });
+                      },
+                    )
                 ],
               ),
             ),
